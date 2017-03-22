97b9b3b3658bfb74ab9e2cbe0d16314942b08421
Lucy Zhang <spothorse9.lucy@gmail.com>
2017-03-22 05:31:08 -0400

    Fade the cube

--- static/js/cube.js
+++ static/js/cube.js
@@ -4,28 +4,40 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.getElementById("cube").appendChild(renderer.domElement);
 var geometry = new THREE.BoxGeometry(500, 500, 500, 10, 10, 10);
-var material = new THREE.MeshBasicMaterial({ color: 0xfffff, wireframe: true });
+var material = new THREE.MeshBasicMaterial({ color: 0x56a0d3, wireframe: true });
 var cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 camera.position.z = 950;
+var increment = 0.01;
 
-
-function render() {
-    requestAnimationFrame(render);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+function render(speed) {
+    cubeAnimationID = requestAnimationFrame(function() {
+        render(speed);
+    });
+    cube.rotation.x += increment;
+    cube.rotation.y += increment;
+    increment += speed;
     renderer.render(scene, camera);
 };
 $(".title").hide();
 $(document).ready(function() {
-	$(".title").show();
+
+    render(0.001);
+
+    setTimeout(function() {
+
+        $("#cube").fadeOut(1000, function() {
+            cancelAnimationFrame(cubeAnimationID);
+        });
+        titleIn();
+    }, 2000)
+})
+
+function titleIn() {
+    $(".title").show();
     $(".title").addClass("animated flipInX");
     $("#logo_mask").addClass("animated zoomIn");
-
-
-
-    render();
-})
+}
 
 //fade in title text
 //$(".title").fadeIn(1000);

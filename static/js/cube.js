44edeaf9c323136be53a1421f903473941154fc9
Lucy Zhang <spothorse9.lucy@gmail.com>
2017-03-23 00:05:34 -0400

    Working on it

--- static/js/cube.js
+++ static/js/cube.js
@@ -20,6 +20,7 @@ function render(speed) {
     renderer.render(scene, camera);
 };
 $(".title").hide();
+$(".content").hide()
 $(document).ready(function() {
 
     render(0.001);
@@ -28,9 +29,12 @@ $(document).ready(function() {
 
         $("#cube").fadeOut(1000, function() {
             cancelAnimationFrame(cubeAnimationID);
+            titleIn();
+            $("#cubeRow").remove();
+            appendParticles();
         });
-        titleIn();
-    }, 2000)
+
+    }, 1700)
 })
 
 function titleIn() {
@@ -39,5 +43,125 @@ function titleIn() {
     $("#logo_mask").addClass("animated zoomIn");
 }
 
-//fade in title text
-//$(".title").fadeIn(1000);
+function appendParticles() {
+    $('body').prepend('<div id="particles-js">');
+    $('body').append('</div>');
+    particlesJS("particles-js", {
+        "particles": {
+            "number": {
+                "value": 33,
+                "density": {
+                    "enable": true,
+                    "value_area": 1420.4657549380909
+                }
+            },
+            "color": {
+                "value": "#56a0d3"
+            },
+            "shape": {
+                "type": "triangle",
+                "stroke": {
+                    "width": 0,
+                    "color": "#56a0d3"
+                },
+                "polygon": {
+                    "nb_sides": 4
+                },
+                "image": {
+                    "src": "img/github.svg",
+                    "width": 100,
+                    "height": 100
+                }
+            },
+            "opacity": {
+                "value": 0.06313181133058181,
+                "random": false,
+                "anim": {
+                    "enable": false,
+                    "speed": 1,
+                    "opacity_min": 0.1,
+                    "sync": false
+                }
+            },
+            "size": {
+                "value": 11.83721462448409,
+                "random": true,
+                "anim": {
+                    "enable": false,
+                    "speed": 40,
+                    "size_min": 0.1,
+                    "sync": false
+                }
+            },
+            "line_linked": {
+                "enable": true,
+                "distance": 150,
+                "color": "#ffffff",
+                "opacity": 0.4,
+                "width": 1
+            },
+            "move": {
+                "enable": true,
+                "speed": 6,
+                "direction": "none",
+                "random": false,
+                "straight": false,
+                "out_mode": "out",
+                "bounce": false,
+                "attract": {
+                    "enable": false,
+                    "rotateX": 600,
+                    "rotateY": 1200
+                }
+            }
+        },
+        "interactivity": {
+            "detect_on": "canvas",
+            "events": {
+                "onhover": {
+                    "enable": true,
+                    "mode": "repulse"
+                },
+                "onclick": {
+                    "enable": true,
+                    "mode": "push"
+                },
+                "resize": true
+            },
+            "modes": {
+                "grab": {
+                    "distance": 400,
+                    "line_linked": {
+                        "opacity": 1
+                    }
+                },
+                "bubble": {
+                    "distance": 400,
+                    "size": 40,
+                    "duration": 2,
+                    "opacity": 8,
+                    "speed": 3
+                },
+                "repulse": {
+                    "distance": 200,
+                    "duration": 0.4
+                },
+                "push": {
+                    "particles_nb": 4
+                },
+                "remove": {
+                    "particles_nb": 2
+                }
+            }
+        },
+        "retina_detect": true
+    });
+}
+
+
+function addContent(){
+	$(".content").show();
+	$(".content").addClass("animated bounceInDown");
+	
+	$(".resourcetitle").addClass("animated bounceInDown");
+}

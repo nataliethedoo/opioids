bd483d396e104e9eeb11f499f5a4225fbe1816c7
Lucy Zhang <spothorse9.lucy@gmail.com>
2017-03-23 20:15:23 -0400

    Updated local storage

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
     triggerAnimation();
-    localStorage.removeItem("first_time");
+    //localStorage.removeItem("first_time");
     var firstTime = localStorage.getItem("first_time");
     if (!firstTime) {
         // first time loaded!
@@ -27,6 +27,13 @@ $(document).ready(function() {
 
 function makeContentVisible() {
 
+}
+
+function initialContent() {
+    titleIn();
+    //$("#cubeRow").remove();
+    appendParticles();
+    addContent();
 }
 
 function triggerAnimation() {
